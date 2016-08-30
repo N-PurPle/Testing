b9c00a1a9ed7d06340cf6d0618276ede16727d80
Nishii Takahiro <nishii@bullets.co.jp>
2016-08-30 15:06:34 +0900

    initial commit

---
+++ tests/func_test.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <testing/testing.hpp>
+
+
+int main(int argc,const char* argv[])
+{
+    using namespace std;
+
+    cout << "Hello World!!" << endl;
+
+    func();
+
+    return 0;
+}
+
